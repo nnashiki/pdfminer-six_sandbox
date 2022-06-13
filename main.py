71b80e633628903b0be90f5ee4ba8289924e4333
Niten Nashiki <niten.nashiki@lightz-inc.com>
2022-06-13 14:53:17 +0900

    update: 改行コードも含めた中身を抽出することができた

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from io import StringIO
-from pdfminer.high_level import extract_text_to_fp
+from pdfminer.high_level import extract_text_to_fp, extract_text
+
 output_string = StringIO()
 with open('data/example.pdf', 'rb') as fin:
     extract_text_to_fp(fin, output_string, output_dir='out_images')
-print(output_string.getvalue())
+
+text = extract_text('data/example.pdf')
+print(repr(text.strip()))
